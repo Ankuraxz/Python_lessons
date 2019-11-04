9e8bf7f7e1fa3c5043f9c9c19e62d102384b87fc
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2019-11-04 14:55:32 +0530

    Create python Class 5B.py

---
+++ python Class 5B.py
@@ -0,0 +1,19 @@
+#intendation is space
+
+#The statements with the same indentation belong to the same group called a suite. 
+
+# a = 1
+# b = 1
+# if a ==1:
+#   print(a,"\n")
+#   if b ==1:
+#     print(b)
+#   print(a-b)
+# print(a+b)
+
+# Spacebarspace : " "
+#tabspace: "  "
+
+# a=1
+#   # print(a)
+
